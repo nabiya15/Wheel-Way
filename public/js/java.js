71b236c7ffc750c2c2fca21cb36e317f617b7ddb
mikcro <nowitsforever@gmail.com>
2017-11-20 20:19:49 -0600

    FrontEnd

---
+++ public/js/java.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     // Handler for .ready() called.
+//     (".button-collapse").sideNav();
+//     console.log("Working");
+// });
+// // Initialize collapse button
+// $(".button-collapse").sideNav();
+// // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+// //$('.collapsible').collapsible();
