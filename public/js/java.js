12c0bc90788b535ddaf60ad7f6826000034fc4d7
mikcro <nowitsforever@gmail.com>
2017-11-27 20:18:34 -0600

    adding public folder

---
+++ public/js/java.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//     // Handler for .ready() called.
+//     (".button-collapse").sideNav();
+//     console.log("Working");
+// });
+// // Initialize collapse button
+// $(".button-collapse").sideNav();
+// // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+// //$('.collapsible').collapsible();
+$(document).ready(function() {
+    $('.materialboxed').materialbox();
+});
+// Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
